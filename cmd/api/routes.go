5a68102d2d1a617a9900512b09d3e57fec05449c
abhishek622 <abhishekprasad0602@gmail.com>
2025-07-11 23:48:52 +0530

    chapter:12 - setting up users

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -19,5 +19,8 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
 
+	// Users
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+
 	return app.recoverPanic(app.rateLimit(router))
 }
